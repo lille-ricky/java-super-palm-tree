159a3152df9a0c52f088a592800e4ec9245aff70
enno <epashaj19@gmail.com>
2025-03-01 21:33:03 +0100

    Image in a window

--- TP5/src/Exo1b.java
+++ TP5/src/Exo1b.java
@@ -1,32 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
-public class Exo1 {
+public class Exo1b {
     public static void main(String[] args) {
-        // Création de la fenêtre
         JFrame fenetre = new JFrame("Une fenêtre");
-        
-        // Obtention de la taille de l'écran
+
         Dimension tailleEcran = Toolkit.getDefaultToolkit().getScreenSize();
         int largeur = tailleEcran.width;
         int hauteur = tailleEcran.height;
         String dimEcran = largeur + " x " + hauteur;
         
-        // Création et ajout du JLabel à la fenêtre
-        JLabel label = new JLabel("Taille de l'écran : "+dimEcran);
+
+        URL url = Exo1b.class.getResource("images/Bird2.gif");
+        JLabel label = new JLabel(new ImageIcon(url));
+
+        
         fenetre.getContentPane().add(label);
         
-        // Ajustement de la taille de la fenêtre selon son contenu
+
         fenetre.pack();
         
-        // Placement de la fenêtre au centre de l'écran
+
         fenetre.setLocation((tailleEcran.width - fenetre.getWidth()) / 2, 
                            (tailleEcran.height - fenetre.getHeight()) / 2);
         
-        // Configuration de la fermeture de la fenêtre
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        // Affichage de la fenêtre
         fenetre.setVisible(true);
     }
 }
