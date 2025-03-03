8bbe32d3ec240f47a7800d311b52516b1a00fce0
enno <epashaj19@gmail.com>
2025-03-03 19:49:57 +0100

    Images are working babyyy

--- TP5/src/MaPremiereAppliGraphique.java
+++ TP5/src/MaPremiereAppliGraphique.java
@@ -3,47 +3,48 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MaPremiereAppliGraphique {
-    
-    private static final ImageIcon[] TABIMAGES = {
-        new ImageIcon("images/Bird.gif"),
-        new ImageIcon("images/Bird2.gif"),
-        new ImageIcon("images/Cat.gif"),
-        new ImageIcon("images/Cat2.gif"),
-        new ImageIcon("images/Dog.gif"),
-        new ImageIcon("images/Dog2.gif"),
-        new ImageIcon("images/Pig.gif")
+    public static final ImageIcon[] TABIMAGES = {
+        new ImageIcon("bin/images/Bird.gif"),
+        new ImageIcon("bin/images/Bird2.gif"),
+        new ImageIcon("bin/images/Cat.gif"),
+        new ImageIcon("bin/images/Cat2.gif"),
+        new ImageIcon("bin/images/Dog.gif"),
+        new ImageIcon("bin/images/Dog2.gif"),
+        new ImageIcon("bin/images/Rabbit.gif")
     };
     
-    private int currentImageIndex = 0;
-    private JLabel imageLabel;
-    
-    public MaPremiereAppliGraphique() {
+    public static void main(String[] args) {
         JFrame fenetre = new JFrame("Les images");
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setSize(400, 300);
         
-        imageLabel = new JLabel(TABIMAGES[currentImageIndex]);
+        JButton leBtn = new JButton(TABIMAGES[0]);
         
-        imageLabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Passage à l'image suivante
-                currentImageIndex = (currentImageIndex + 1) % TABIMAGES.length;
-                imageLabel.setIcon(TABIMAGES[currentImageIndex]);
-            }
-        });
+        fenetre.getContentPane().add(leBtn, BorderLayout.CENTER);
         
-        fenetre.getContentPane().add(imageLabel, BorderLayout.CENTER);
+        leBtn.addActionListener(new GereClickBouton(leBtn));
         
-        fenetre.pack();
-        fenetre.setLocationRelativeTo(null);
         fenetre.setVisible(true);
     }
+}
+
+class GereClickBouton implements ActionListener {
+    private int compteur;
+    private JButton btn;
     
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new MaPremiereAppliGraphique();
-            }
-        });
+    public GereClickBouton(JButton btn) {
+        this.compteur = 0;
+        this.btn = btn;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        compteur++;
+        
+        if (compteur >= MaPremiereAppliGraphique.TABIMAGES.length) {
+            compteur = 0;
+        }
+        
+        btn.setIcon(MaPremiereAppliGraphique.TABIMAGES[compteur]);
     }
 }
